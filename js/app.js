a78e9cdb282d89f73bc83ac94900a2ecf972f10b
MD FARHAD ALI <farhads.creation@gmail.com>
2022-09-01 02:22:15 +0600

    first commit

--- js/app.js
+++ js/app.js
@@ -1,16 +1,24 @@
-const loadPhones = async(searchText) => {
+const loadPhones = async(searchText, dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res = await fetch (url);
     const data = await res.json();
-    displayPhones(data.data)
+    displayPhones(data.data, dataLimit)
 }
 
 // এখানে আমরা ফর ইস ব্যবহার করে ফোন এর ইনফরমেশন গুলো ডম এর সাথে কানেক্ট করে ডাইনামিক করেছি। 
-const displayPhones = phones => {
+const displayPhones = (phones, dataLimit) => {
     const phonesContainer = document.getElementById('phone-container');
     phonesContainer.innerText = '';
-    // display 20 phone only
-    phones = phones.slice(0, 20)
+    // display 10 phone only
+    const loadMore = document.getElementById('load-more');
+    if(dataLimit && phones.length > 10){
+        phones = phones.slice(0, 10)
+        loadMore.classList.remove('d-none'); 
+    }
+
+    else{
+        loadMore.classList.add('d-none')
+    }
     // display no phone found
     const noPhone = document.getElementById('no-phone-found')
     if(phones.length === 0){
@@ -30,7 +38,7 @@ const displayPhones = phones => {
         <div class="card-body">
         <h5 class="card-title">${phone.phone_name}</h5>
         <h5>Brand: ${phone.brand}</h5>
-        <h6 class="card-text">Slug: ${phone.slug}</h6>
+        <button href="#" class="btn btn-primary mt-3">Go somewhere</Button>
         </div>
         </div>
     </div>
@@ -41,17 +49,18 @@ const displayPhones = phones => {
     toggleSpinner(false)
 }
 
+const processSearch = (dataLimit) =>{ 
+    toggleSpinner(true);
+    const searchField = document.getElementById('search-field');
+    const searchText = searchField.value;
+    loadPhones(searchText, dataLimit);
+}
+
 // এবারে আমরা সার্চ ফাংশন করব।
 // get phone button
 // handle search button click
 document.getElementById('btn-search').addEventListener('click', function(){
-    // start loader
-    toggleSpinner(true);
-
-    const searchField = document.getElementById('search-field');
-    const searchText = searchField.value;
-    loadPhones(searchText);
-
+    processSearch(10);
 })
 
 // loader function
@@ -64,5 +73,10 @@ const toggleSpinner = isLoading => {
         loaderSection.classList.add('d-none')
     }
 }
+
+// create load more system 
+document.getElementById('btn-load-more').addEventListener('click', function(){
+    processSearch();
+})
 
 // loadPhones();
